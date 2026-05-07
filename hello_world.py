0c0ba0b92bfa2b0e2b0636519216886f7495f3d4
Harshit <jokergoat001@gmail.com>
2026-05-08 02:14:03 +0530

    User input first program

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,29 @@
 print("Hello, World!")
-a = 20
+a = 19
 b = 89.69
+c = 65j
+st = 'jkdsi *&*&&'
+d = True
+e = False
 print(a+b)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(st))
+print(type(d))
+print(type(e))
+
+#a = 33
+#print(chr(a))
+
+f = "Harshit"
+print(f[1:6:1])
+
+g = 0
+g = bool(g)
+print(g)
+
+h = 12
+print(h/3)
+
+print(f,a)
---
+++ user_input.py
@@ -0,0 +1,2 @@
+age = int(input("Hello what is your age: "))
+print(age)
