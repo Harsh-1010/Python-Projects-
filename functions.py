5c2ce2a910883659c6a79e549984e9ae6d14dd73
Harshit <jokergoat001@gmail.com>
2026-05-13 23:20:46 +0530

    Data Structures

--- functions.py
+++ functions.py
@@ -1,5 +1,22 @@
-def sum(a,b):
-    print(f"The sum of two no. is {a+b}")
+"""Function"""
+# def sum(a,b):
+#     print(f"The sum of two no. is {a+b}")
 
-sum(12,78)
-sum(45,55)
+# sum(12,78)
+# sum(45,55)
+# sum(95,95)
+
+# """Postional Argument"""
+# def sum (a,b):
+#     print(a + b)
+# sum(12,14)
+
+# """Keyword Argument"""
+# def info(name, age):
+#     print(f"Hello my name is {name} and my age is {age}")
+# info(age = 20, name = "Harshit")
+
+# """Default Argument"""
+# def add(a, b=56):
+#     print(a + b)
+# add(12)
